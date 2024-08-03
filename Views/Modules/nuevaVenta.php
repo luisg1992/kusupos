02e4244f2a800c235e7b83cf30c54b2ebd7beda4
Luis Gonzales <luis@allmoxy.com>
2024-08-03 10:37:02 -0500

    mysql

--- Views/Modules/nuevaVenta.php
+++ Views/Modules/nuevaVenta.php
@@ -38,9 +38,134 @@
                         </div>
                     </div>    
                 </div>
-                
+
+                <div class="row">
+                    <div class="col-lg-2">
+                        <button class="btn btn-block bg-gradient-primary" data-toggle="modal" data-target="#modalAgregarArticulo">Agregar Articulo</button>
+                    </div>
+                </div>
+                <div class="card-body">
+          <table id="datatable" class="table table-bordered table-striped">
+            <thead>
+              <tr>
+                <th style="width: 10px;">#</th>
+                <th>Articulo</th>
+                <th>Cantidad</th>
+                <th>Descripcion</th>
+                <th>Precio</th>
+                <th>Acciones</th>
+              </tr>
+            </thead>
+            <tbody>
+              
+            </tbody>
+          </table>
+        </div>
+        <!-- /.card-body -->
+      </div>
+      <!-- /.card -->
+
+    </section>
+    <!-- /.content -->
+  </div>
 
             </div>
         </div>
     </section>
 </div>
+
+
+<div class="modal fade" id="modalAgregarArticulo">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <form role="form" method="post" enctype="multipart/form-data">  
+              <div class="modal-header" style="background:#3c8dbc; color: white;">
+                <h4 class="modal-title">Agregar Articulo</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+
+              <div class="modal-body">
+                <div class="card-body">
+
+                <div class="form-group">
+                    <label for="nuevoNombre">Articulo</label>
+                    <div class="input-group">
+                      <div class="input-group-prepend">
+                        <span class="input-group-text"><i class="fa fa-user"></i></span>
+                      </div>
+                      <input type="text" name="nuevoNombre" id="nuevoNombre" class="form-control" placeholder="Ingresar Nombre..." required>
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <label for="nuevoUsuario">Cantidad</label>
+                    <div class="input-group">
+                      <div class="input-group-prepend">
+                        <span class="input-group-text"><i class="fa fa-user"></i></span>
+                      </div>
+                      <input type="text" name="nuevoUsuario" id="nuevoUsuario" class="form-control" placeholder="Ingresar Usuario..." required>
+                    </div>
+                  </div>
+                    
+                  <div class="form-group">
+                    <label for="nuevaPassword">Descripcion</label>
+                    <div class="input-group">
+                      <div class="input-group-prepend">
+                        <span class="input-group-text"><i class="fa fa-lock"></i></span>
+                    </div>
+                      <input type="password" name="nuevaPassword" class="form-control" id="nuevaPassword" placeholder="Ingrese Contraseña..." required>
+                    </div>  
+                  </div>
+
+                  <div class="form-group">
+                    <label for="perfil">Precio</label>
+                    <div class="input-group">
+                      <div class="input-group-prepend">
+                        <span class="input-group-text"><i class="fa fa-users"></i></span>
+                      </div>
+                      <select class="form-control" name="nuevoPerfil" id="nuevoPerfil">
+                        <option value="">--SELECCIONAR PERFIL--</option>
+                        <option value="1">Administrador</option>
+                        <option value="2">Especial</option>
+                        <option value="3">Vendedor</option>
+                      </select>
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <label for="nuevaFoto">File input</label>
+                    <div class="input-group">
+                      <div class="custom-file">
+                        <input type="file" id="nuevaFoto" name="nuevaFoto" class="nuevaFoto">
+                      </div>
+                    </div>
+                    <img src="Views/img/default/avatar.png" class="img-thumbnail previsualizar" width="100px;">
+                    <p class="help-block">Peso máximo de la foto 2 MB</p>
+                  </div>
+                </div>
+                <!-- /.card-body -->
+              </div>
+              <!-- /.card -->
+              
+              <div class="modal-footer justify-content-between">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Salir</button>
+                <button type="submit" class="btn btn-primary">Guardar Usuario</button>
+              </div>
+              
+              <?php
+
+                /*   $crearUsuario = new ControllerUsuarios();
+                  $crearUsuario -> ctrCrearUsuario(); */
+
+              
+              ?>
+            </form>
+          </div>
+          <!-- /.modal-content -->
+        </div>
+        <!-- /.modal-dialog -->
+      </div>
+      <!-- /.modal -->
+¿
--- kusupos.sql
+++ kusupos.sql
@@ -11,11 +11,50 @@
  Target Server Version : 100428
  File Encoding         : 65001
 
- Date: 16/06/2024 08:50:57
+ Date: 03/08/2024 10:36:48
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for articulos
+-- ----------------------------
+DROP TABLE IF EXISTS `articulos`;
+CREATE TABLE `articulos`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `precio` float NOT NULL,
+  `idCategoria` int NOT NULL,
+  `estado` int NOT NULL,
+  `foto` varchar(255) CHARACTER SET utf8 COLLATE utf8_spanish_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `idCategoria`(`idCategoria`) USING BTREE,
+  CONSTRAINT `articulos_ibfk_1` FOREIGN KEY (`idCategoria`) REFERENCES `categorias` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE = InnoDB AUTO_INCREMENT = 38 CHARACTER SET = utf8 COLLATE = utf8_spanish_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of articulos
+-- ----------------------------
+INSERT INTO `articulos` VALUES (1, 'Maki', 25, 1, 1, NULL);
+INSERT INTO `articulos` VALUES (2, '20 Cortes', 46, 2, 1, NULL);
+INSERT INTO `articulos` VALUES (3, '30 Cortes', 68, 2, 1, NULL);
+INSERT INTO `articulos` VALUES (4, '50 Cortes', 100, 2, 1, NULL);
+INSERT INTO `articulos` VALUES (5, '60 Cortes', 115, 2, 1, NULL);
+INSERT INTO `articulos` VALUES (6, 'Poke', 28, 3, 1, NULL);
+INSERT INTO `articulos` VALUES (7, 'Poke + Gaseosa', 30, 3, 1, NULL);
+INSERT INTO `articulos` VALUES (8, 'Promo Poke x2', 50, 3, 1, NULL);
+INSERT INTO `articulos` VALUES (9, 'Promo Poke x 2 + Gaseosa', 55, 3, 1, NULL);
+INSERT INTO `articulos` VALUES (10, 'Sashimi', 18, 4, 1, NULL);
+INSERT INTO `articulos` VALUES (11, 'Nigiri', 18, 4, 1, NULL);
+INSERT INTO `articulos` VALUES (12, 'Tiradito', 35, 4, 1, NULL);
+INSERT INTO `articulos` VALUES (13, 'Taquitos Nikkei', 25, 4, 1, NULL);
+INSERT INTO `articulos` VALUES (14, 'Ebi Furai x6', 18, 4, 1, NULL);
+INSERT INTO `articulos` VALUES (15, 'Ebi Furai x12', 30, 4, 1, NULL);
+INSERT INTO `articulos` VALUES (16, 'Bebidas', 5, 5, 1, NULL);
+INSERT INTO `articulos` VALUES (17, 'Pilsen', 7, 6, 1, NULL);
+INSERT INTO `articulos` VALUES (18, 'Heineken', 8, 6, 1, NULL);
+INSERT INTO `articulos` VALUES (19, 'Cremas', 3, 6, 1, NULL);
 
 -- ----------------------------
 -- Table structure for categorias
@@ -31,10 +70,11 @@ CREATE TABLE `categorias`  (
 -- Records of categorias
 -- ----------------------------
 INSERT INTO `categorias` VALUES (1, 'Makis');
-INSERT INTO `categorias` VALUES (2, 'Pokebowls');
-INSERT INTO `categorias` VALUES (3, 'Cortes');
-INSERT INTO `categorias` VALUES (4, 'Bebidas');
-INSERT INTO `categorias` VALUES (5, 'Adicionales');
+INSERT INTO `categorias` VALUES (2, 'Promo');
+INSERT INTO `categorias` VALUES (3, 'Pokes');
+INSERT INTO `categorias` VALUES (4, 'Cortes');
+INSERT INTO `categorias` VALUES (5, 'Bebidas');
+INSERT INTO `categorias` VALUES (6, 'Adicionales');
 
 -- ----------------------------
 -- Table structure for clientes
@@ -53,6 +93,39 @@ CREATE TABLE `clientes`  (
 -- ----------------------------
 
 -- ----------------------------
+-- Table structure for poke
+-- ----------------------------
+DROP TABLE IF EXISTS `poke`;
+CREATE TABLE `poke`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) CHARACTER SET utf8 COLLATE utf8_spanish_ci NULL DEFAULT NULL,
+  `tipo` varchar(255) CHARACTER SET utf8 COLLATE utf8_spanish_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 19 CHARACTER SET = utf8 COLLATE = utf8_spanish_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of poke
+-- ----------------------------
+INSERT INTO `poke` VALUES (1, 'Arroz Maki', 'Base');
+INSERT INTO `poke` VALUES (2, 'Mix Lechugas', 'Base');
+INSERT INTO `poke` VALUES (3, 'Bonito', 'Proteina');
+INSERT INTO `poke` VALUES (4, 'Trucha', 'Proteina');
+INSERT INTO `poke` VALUES (5, 'Pescado de Temporada', 'Proteina');
+INSERT INTO `poke` VALUES (6, 'Chicken Katsu', 'Proteina');
+INSERT INTO `poke` VALUES (7, 'Pollo Karage', 'Proteina');
+INSERT INTO `poke` VALUES (8, 'Palta', 'Toppin');
+INSERT INTO `poke` VALUES (9, 'Kiuri', 'Toppin');
+INSERT INTO `poke` VALUES (10, 'Zanahorita', 'Toppin');
+INSERT INTO `poke` VALUES (11, 'Hojuelas de Camaron', 'Toppin');
+INSERT INTO `poke` VALUES (12, 'Col con tartara', 'Toppin');
+INSERT INTO `poke` VALUES (13, 'Holantao', 'Toppin');
+INSERT INTO `poke` VALUES (14, 'Tomate Picado', 'Toppin');
+INSERT INTO `poke` VALUES (15, 'Cebolla China', 'Add On');
+INSERT INTO `poke` VALUES (16, 'Ajonjoli', 'Add On');
+INSERT INTO `poke` VALUES (17, 'Nori', 'Add On');
+INSERT INTO `poke` VALUES (18, 'Togarashi', 'Add On');
+
+-- ----------------------------
 -- Table structure for productos
 -- ----------------------------
 DROP TABLE IF EXISTS `productos`;
@@ -66,7 +139,7 @@ CREATE TABLE `productos`  (
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `idCategoria`(`idCategoria`) USING BTREE,
   CONSTRAINT `productos_ibfk_1` FOREIGN KEY (`idCategoria`) REFERENCES `categorias` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE = InnoDB AUTO_INCREMENT = 15 CHARACTER SET = utf8 COLLATE = utf8_spanish_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 22 CHARACTER SET = utf8 COLLATE = utf8_spanish_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of productos
@@ -85,6 +158,10 @@ INSERT INTO `productos` VALUES (11, 'Ostion', 25, 1, 1, 'Assets\\img\\productos\
 INSERT INTO `productos` VALUES (12, 'Ice Cream', 25, 1, 1, 'Assets\\img\\productos\\makis\\icecream.jpg');
 INSERT INTO `productos` VALUES (13, 'Dulce', 25, 1, 1, 'Assets\\img\\productos\\makis\\dulce.jpg');
 INSERT INTO `productos` VALUES (14, 'Vegetariano', 25, 1, 1, 'Assets\\img\\productos\\makis\\vegetariano.jpg');
+INSERT INTO `productos` VALUES (15, '20 Cortes', 46, 2, 1, 'Assets\\img\\default\\noimage.jpg');
+INSERT INTO `productos` VALUES (16, '30 Cortes', 68, 2, 1, 'Assets\\img\\default\\noimage.jpg');
+INSERT INTO `productos` VALUES (17, '50 Cortes', 100, 2, 1, 'Assets\\img\\default\\noimage.jpg');
+INSERT INTO `productos` VALUES (18, '60 Cortes', 115, 2, 1, 'Assets\\img\\default\\noimage.jpg');
 
 -- ----------------------------
 -- Table structure for tipodocumento
@@ -109,14 +186,15 @@ CREATE TABLE `ventas`  (
   `idCliente` int NULL DEFAULT NULL,
   `total` float NOT NULL,
   `formaPago` int NOT NULL,
-  `nTransacción` varchar(255) CHARACTER SET utf8 COLLATE utf8_spanish_ci NULL DEFAULT NULL,
+  `nTransaccion` varchar(255) CHARACTER SET utf8 COLLATE utf8_spanish_ci NULL DEFAULT NULL,
   `FechaHora` timestamp(0) NOT NULL DEFAULT current_timestamp(0) ON UPDATE CURRENT_TIMESTAMP(0),
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_spanish_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_spanish_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of ventas
 -- ----------------------------
+INSERT INTO `ventas` VALUES (1, 1, 50, 0, '12345', '2024-08-02 11:55:41');
 
 -- ----------------------------
 -- Table structure for ventasdetalle
